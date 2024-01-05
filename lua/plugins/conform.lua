3ff7911405295e6a85f0d3c23dab17f5e3a7bc27
devlevic <contact@devlevi.com>
2024-01-05 18:35:39 -0300

    initial setup

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,26 @@
+-- Docs https://www.lazyvim.org/plugins/formatting
+local opts = {
+  -- LazyVim will use these options when formatting with the conform.nvim formatter
+  format = {
+    timeout_ms = 3000,
+    async = false, -- not recommended to change
+    quiet = false, -- not recommended to change
+  },
+
+  ---@type table<string, conform.FormatterUnit[]>
+  formatters_by_ft = {
+    lua = { "stylua" },
+    sh = { "shfmt" },
+    typescript = { "prettierd" },
+  },
+
+  ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
+  formatters = {
+    injected = { options = { ignore_errors = true } },
+  },
+}
+
+return {
+  "stevearc/conform.nvim",
+  opts = opts,
+}
