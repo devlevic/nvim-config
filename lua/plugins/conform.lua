7cf78e197d250723ba7439c51e235a0c97e1c086
devlevic <contact@devlevi.com>
2024-01-08 20:33:18 -0300

    feat: added biome as a default formatter and linter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,21 +1,25 @@
 -- Docs https://www.lazyvim.org/plugins/formatting
 local opts = {
   -- LazyVim will use these options when formatting with the conform.nvim formatter
-  format = {
-    timeout_ms = 3000,
-    async = false, -- not recommended to change
-    quiet = false, -- not recommended to change
-  },
+  -- format = {
+  --   timeout_ms = 3000,
+  --   async = false, -- not recommended to change
+  --   quiet = false, -- not recommended to change
+  -- },
 
   ---@type table<string, conform.FormatterUnit[]>
   formatters_by_ft = {
-    typescript = { "prettierd" },
+    javascript = { "biome" },
+    javascriptreact = { "biome" },
+    typescript = { "biome" },
+    typescriptreact = { "biome" },
+    json = { "biome" },
   },
 
-  ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
-  formatters = {
-    injected = { options = { ignore_errors = true } },
-  },
+  -- ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
+  -- formatters = {
+  --   injected = { options = { ignore_errors = true } },
+  -- },
 }
 
 return {
--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,16 +1,14 @@
 return {
   "mfussenegger/nvim-lint",
   opts = {
-    -- Event to trigger linters
     events = { "BufWritePost", "BufReadPost", "InsertLeave" },
     linters_by_ft = {
       fish = { "fish" },
-      typescript = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
-      -- Use the "*" filetype to run linters on all filetypes.
-      -- ['*'] = { 'global linter' },
-      -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
-      -- ['_'] = { 'fallback linter' },
+      typescript = { "biomejs" },
+      typescriptreact = { "biomejs" },
+      javascript = { "biomejs" },
+      javascriptreact = { "biomejs" },
+      json = { "biomejs" },
     },
     ---@type table<string,table>
     linters = {},
