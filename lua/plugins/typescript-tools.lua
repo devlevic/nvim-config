6aa720692b23fc8943d364f25f844ff260e68ad7
devlevic <contact@devlevi.com>
2024-01-08 20:33:37 -0300

    refact: made changes to decrease tsserver memory usage

--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -1,5 +1,20 @@
 return {
   "pmizio/typescript-tools.nvim",
   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  opts = {},
+  config = function()
+    require("typescript-tools").setup({
+      settings = {
+        tsserver_path = "/usr/lib/code/extensions/node_modules/typescript/lib/tsserver.js",
+        expose_as_code_action = { "add_missing_imports" },
+        publish_diagnostic_on = "insert_leave",
+        separate_diagnostic_server = false,
+        code_lens = "off",
+        disable_member_code_lens = true,
+        tsserver_max_memory = "3072",
+        jsx_close_tag = {
+          enable = false,
+        },
+      },
+    })
+  end,
 }
