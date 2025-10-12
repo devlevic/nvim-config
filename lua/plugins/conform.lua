1f9f1a67dc58f01e110f360bdb4a3e3318a92071
devlevic <contact@devlevi.com>
2025-10-12 10:51:52 -0300

    chore: update deps

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,29 +6,29 @@ utils.set_fallback(fallback)
 local setup_formatter_by_config_file = utils.setup_formatter_by_config_file(config_files_in_order_js)
 
 -- Docs https://www.lazyvim.org/plugins/formatting
-local opts = {
-  -- LazyVim will use these options when formatting with the conform.nvim formatter
-  default_format_options = {
+local opts = function(_, opts)
+  opts.default_format_options = {
     timeout_ms = 3000,
     async = false, -- not recommended to change
     quiet = false, -- not recommended to change
-  },
+  }
 
   ---@type table<string, string[] | function>
-  formatters_by_ft = {
-    -- json = { "biome" },
-    typescriptreact = setup_formatter_by_config_file,
-    typescript = setup_formatter_by_config_file,
-    javascript = setup_formatter_by_config_file,
-    javascriptreact = setup_formatter_by_config_file,
-    go = { "gofmt" },
-  },
+  opts.formatters_by_ft = opts.formatters_by_ft
 
-  -- ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
-  -- formatters = {
-  --   injected = { options = { ignore_errors = true } },
-  -- },
-}
+  opts.terraform = { "terraform" }
+  opts.tf = { "terraform" }
+  opts.typescriptreact = setup_formatter_by_config_file
+  opts.typescript = setup_formatter_by_config_file
+  opts.javascript = setup_formatter_by_config_file
+  opts.javascriptreact = setup_formatter_by_config_file
+  opts.go = { "gofmt" }
+  opts.json = { "biome" }
+
+  opts.formatters = {
+    injected = { options = { ignore_errors = true } },
+  }
+end
 
 vim.keymap.set("n", "<leader>cL", function()
   print("current formatter: " .. utils.current_formatter)
